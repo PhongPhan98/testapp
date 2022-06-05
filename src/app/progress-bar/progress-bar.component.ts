d999dba59a5e0e949235bb8a7eafb932dbc12fc6
phongphan <phongpx9x@gmail.com>
2022-06-05 15:12:35 +0700

    [Phong.Phan] Day 7 | Bắt sự kiện khi thuộc tính của @Input() thay đổi => Using ngOnChanges

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -1,14 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-progress-bar',
   templateUrl: './progress-bar.component.html',
   styleUrls: ['./progress-bar.component.css']
 })
-export class ProgressBarComponent implements OnInit {
+export class ProgressBarComponent implements OnInit, OnChanges {
   @Input() backgroundColor: string;
   @Input() progressColor: string;
   @Input() progress = 0;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if ("progress" in changes) {
+      const value = changes["progress"].currentValue;
+      if (typeof value !== "number") {
+        const progress = Number(value);
+        if (Number.isNaN(progress)) {
+          this.progress = 0
+        }
+        else {
+          this.progress = progress;
+        }
+
+      }
+    }
+  }
 
   constructor() { }
 
